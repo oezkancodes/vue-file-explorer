de2ce406a3afb82e4eff2184ac0231255c450256
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-12 15:13:47 +0100

    Add basic Main Navigation interaction

--- src/components/Main/MainNavigation.vue
+++ src/components/Main/MainNavigation.vue
@@ -2,8 +2,11 @@
   <aside
     class="flex-shrink-0 p-2 w-52 border-r border-gray-700 flex flex-col space-y-1 overflow-y-auto"
   >
-    <MainNavigationButton label="Desktop" active />
-    <MainNavigationButton label="Downloads" />
+    <MainNavigationButton
+      v-for="item in items"
+      :key="item.label"
+      :item="item"
+    />
   </aside>
 </template>
 
@@ -13,6 +16,26 @@
   export default {
     components: {
       MainNavigationButton,
+    },
+
+    data() {
+      return {
+        items: [
+          {
+            label: 'Favourites',
+            iconComponent: 'StarIcon',
+            folders: [
+              { label: 'Home' },
+              { label: 'Desktop' },
+            ],
+          },
+          {
+            label: 'Cloud',
+            iconComponent: 'CloudIcon',
+            folders: [{ label: 'OneDrive' }],
+          },
+        ],
+      };
     },
   };
 </script>
--- src/components/Main/MainNavigationButton.vue
+++ src/components/Main/MainNavigationButton.vue
@@ -1,22 +1,56 @@
 <template>
-  <button
-    class="px-3 py-2 rounded-md flex items-center space-x-2 text-xs hover:bg-gray-500 hover:bg-opacity-25 transition duration-100"
-    :class="{
-      'bg-gray-500 bg-opacity-25 font-medium': active,
-    }"
-  >
-    <FolderIcon />
-    <span v-if="label">{{ label }} </span>
-    <slot v-else name="default" />
-  </button>
+  <div>
+    <button
+      class="w-full px-3 py-2 rounded-md flex items-center space-x-2 justify-between text-xs hover:bg-gray-500 hover:bg-opacity-25 transition duration-100"
+      :class="{
+        'bg-gray-500 bg-opacity-25 font-medium': active,
+      }"
+      @click="collapsed = !collapsed"
+    >
+      <div class="flex items-center space-x-2">
+        <component
+          :is="item.iconComponent || 'FolderIcon'"
+          class="w-4 h-4"
+        />
+        <span v-text="item.label" />
+      </div>
+
+      <ChevronDownIcon
+        class="w-4 h-4 self-end"
+        :class="{ 'transform rotate-180': !collapsed }"
+      />
+    </button>
+
+    <div v-if="!collapsed" class="">
+      <button
+        v-for="folder in item.folders"
+        :key="folder.key"
+        class="w-full pl-8 pr-3 py-2 rounded-md flex items-center space-x-2 text-xs hover:bg-gray-500 hover:bg-opacity-25 transition duration-100"
+        :class="{
+          'bg-gray-500 bg-opacity-25 font-medium': active,
+        }"
+      >
+        <FolderIcon class="w-4 h-4" />
+        <span v-text="folder.label" />
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-  import { FolderIcon } from '@vue-hero-icons/solid';
+  import {
+    StarIcon,
+    CloudIcon,
+    FolderIcon,
+    ChevronDownIcon,
+  } from '@vue-hero-icons/solid';
 
   export default {
     components: {
+      StarIcon,
+      CloudIcon,
       FolderIcon,
+      ChevronDownIcon,
     },
 
     props: {
@@ -24,10 +58,16 @@
         type: Boolean,
         default: null,
       },
-      label: {
-        type: String,
-        default: null,
+      item: {
+        type: Object,
+        required: true,
       },
+    },
+
+    data() {
+      return {
+        collapsed: false,
+      };
     },
   };
 </script>
