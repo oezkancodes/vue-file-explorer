5d67f2aefb36b3db498e4abbd0a82c528603a700
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-15 19:34:03 +0100

    Adjust minor Spacing

--- src/components/Main/MainContentHeaderButton.vue
+++ src/components/Main/MainContentHeaderButton.vue
@@ -1,14 +1,10 @@
 <template>
   <button
-    class="p-2 rounded-md hover:bg-gray-600 hover:bg-opacity-25 transition duration-100 flex items-center space-x-1"
+    class="p-2 rounded-md hover:bg-gray-600 hover:bg-opacity-25 transition duration-100 flex items-center space-x-1.5"
   >
     <slot name="default" />
   </button>
 </template>
-
-<script>
-  export default {};
-</script>
 
 <style scoped>
   svg {
