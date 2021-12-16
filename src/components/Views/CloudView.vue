d944944b536712779ba7574479184f0b56b56160
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-16 12:48:31 +0100

    Improve Cloud View button

--- src/components/Views/CloudView.vue
+++ src/components/Views/CloudView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <div class="p-4">
+    <div class="p-4 flex">
       <a
-        class="px-4 py-2 border border-gray-700 bg-gray-500 bg-opacity-20 hover:bg-opacity-30 focus:bg-opacity-25 rounded-md transition duration-100 text-sm"
+        class="px-4 py-2 border border-gray-700 bg-gray-500 bg-opacity-20 hover:bg-opacity-30 focus:bg-opacity-25 rounded-md transition duration-100 text-sm flex items-center space-x-1.5"
         href="https://onedrive.live.com/about/de-de/signin/"
         target="_blank"
       >
-        Please connect to OneDrive
+        <span>Connect OneDrive</span>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-4 w-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+          />
+        </svg>
       </a>
     </div>
   </div>
