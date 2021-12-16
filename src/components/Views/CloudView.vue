9f74989a09bcae2e6978857b266bd7089e9cdf3c
Özkan Yanikbas <oezkanyanikbas@gmail.com>
2021-12-16 12:25:32 +0100

    Add Cloud View content

--- src/components/Views/CloudView.vue
+++ src/components/Views/CloudView.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <div class="p-4">⚠️ OneDrive view coming soon</div>
+    <div class="p-4">
+      <a
+        class="px-4 py-2 border border-gray-700 bg-gray-500 bg-opacity-20 hover:bg-opacity-30 focus:bg-opacity-25 rounded-md transition duration-100 text-sm"
+        href="https://onedrive.live.com/about/de-de/signin/"
+        target="_blank"
+      >
+        Please connect to OneDrive
+      </a>
+    </div>
   </div>
 </template>
 
